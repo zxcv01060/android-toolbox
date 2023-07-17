20b2872a56f4ccfc2adaab81b90c771d93cff7c5
Louis Li <a0909002250@gmail.com>
2023-07-17 20:56:37 +0800

    fix: [產生器] 修正UrlShortenerViewModel上面缺少@HiltViewModel annotation的問題

--- app/src/main/java/tw/idv/louislee/toolbox/ui/page/urlshortener/UrlShortenerViewModel.kt
+++ app/src/main/java/tw/idv/louislee/toolbox/ui/page/urlshortener/UrlShortenerViewModel.kt
@@ -1,9 +1,11 @@
 package tw.idv.louislee.toolbox.ui.page.urlshortener
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import tw.idv.louislee.toolbox.service.UrlShortenerService
 import javax.inject.Inject
 
+@HiltViewModel
 class UrlShortenerViewModel @Inject constructor(
     private val service: UrlShortenerService
 ) : ViewModel() {
