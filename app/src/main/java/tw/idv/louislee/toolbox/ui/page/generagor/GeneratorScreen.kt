c8add55f3056783cccfe0aa95b7fc99a52ca5ddb
Louis Li <a0909002250@gmail.com>
2023-07-30 10:20:03 +0800

    feat: [產生器] 主要畫面改為顯示Tab切換功能

--- app/src/main/java/tw/idv/louislee/toolbox/ui/page/generagor/GeneratorScreen.kt
+++ app/src/main/java/tw/idv/louislee/toolbox/ui/page/generagor/GeneratorScreen.kt
@@ -1,21 +1,28 @@
 package tw.idv.louislee.toolbox.ui.page.generagor
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import tw.idv.louislee.toolbox.R
 import tw.idv.louislee.toolbox.ui.AppPreview
 import tw.idv.louislee.toolbox.ui.component.AppToolbar
 import tw.idv.louislee.toolbox.ui.theme.ToolboxTheme
 import java.util.UUID
+
+private const val TAB_UUID = 0
+private const val TAB_DATABASE_CONNECTION_STRING = 1
+private const val TAB_C_SHARP_DB_SCAFFOLD = 2
 
 @Composable
 fun GeneratorScreen(
@@ -32,13 +39,43 @@ private fun Content(drawerState: DrawerState, generateUuid: () -> String) {
             title = stringResource(id = R.string.generator_title),
             drawerState = drawerState
         ) {
-            Column(
-                modifier = Modifier.padding(all = 8.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                UuidGenerator(generateUuid = generateUuid)
+            var selectedTabIndex by remember {
+                mutableStateOf(TAB_UUID)
+            }
+
+            Column {
+                GeneratorTabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    onTabSelected = { selectedTabIndex = it }
+                )
+
+                when (selectedTabIndex) {
+                    TAB_UUID -> UuidGenerator(generateUuid = generateUuid)
+                    else -> Unit
+                }
             }
         }
+    }
+}
+
+@Composable
+private fun GeneratorTabRow(selectedTabIndex: Int, onTabSelected: (Int) -> Unit) {
+    TabRow(selectedTabIndex = selectedTabIndex) {
+        Tab(
+            selected = selectedTabIndex == TAB_UUID,
+            text = { Text(text = stringResource(id = R.string.generator_uuid_title)) },
+            onClick = { onTabSelected(TAB_UUID) }
+        )
+        Tab(
+            selected = selectedTabIndex == TAB_DATABASE_CONNECTION_STRING,
+            text = { Text(text = stringResource(id = R.string.generator_database_connection_string_title)) },
+            onClick = { onTabSelected(TAB_DATABASE_CONNECTION_STRING) }
+        )
+        Tab(
+            selected = selectedTabIndex == TAB_C_SHARP_DB_SCAFFOLD,
+            text = { Text(text = stringResource(id = R.string.generator_c_sharp_db_scaffold_title)) },
+            onClick = { onTabSelected(TAB_C_SHARP_DB_SCAFFOLD) }
+        )
     }
 }
 
