76f89ffddce1c930ac4a86d9af45aec06cb886cd
Louis Li <a0909002250@gmail.com>
2023-07-31 20:39:18 +0800

    fix: [產生器] 修正UUID新增後，不會自動捲到最新一筆UUID的問題

--- app/src/main/java/tw/idv/louislee/toolbox/ui/page/generagor/UuidGenerator.kt
+++ app/src/main/java/tw/idv/louislee/toolbox/ui/page/generagor/UuidGenerator.kt
@@ -6,15 +6,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import tw.idv.louislee.toolbox.R
 import tw.idv.louislee.toolbox.ui.AppPreview
 import tw.idv.louislee.toolbox.ui.component.button.AppCopyButton
@@ -27,17 +29,19 @@ fun UuidGenerator(generateUuid: () -> String) {
         val uuidHistories = remember {
             mutableStateListOf<String>()
         }
+        val lazyListState = rememberLazyListState()
+        val coroutineScope = rememberCoroutineScope()
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            Button(onClick = { uuidHistories.add(generateUuid()) }) {
-                Text(text = stringResource(id = R.string.generator_generate))
+        Button(onClick = {
+            uuidHistories.add(generateUuid())
+            coroutineScope.launch {
+                lazyListState.scrollToItem(index = uuidHistories.lastIndex)
             }
+        }) {
+            Text(text = stringResource(id = R.string.generator_generate))
         }
 
-        LazyColumn(reverseLayout = true) {
+        LazyColumn(state = lazyListState, reverseLayout = true) {
             itemsIndexed(uuidHistories) { i, uuid ->
                 UuidRow(id = i + 1, uuid = uuid)
             }
